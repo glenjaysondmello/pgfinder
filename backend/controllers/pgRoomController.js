6cd9d978fdc93614d2e33758387d99263fa0d1ab
Glen Jayson Dmello <glendmello04@gmail.com>
2025-02-26 18:49:52 +0530

    modified update pg logic for handling deleting and adding images

--- backend/controllers/pgRoomController.js
+++ backend/controllers/pgRoomController.js
@@ -38,11 +38,33 @@ const addPg = async (req, res) => {
 
 const updatePg = async (req, res) => {
   try {
-    const imageUrls = req.files.map((file) => file.path);
+    const room = await PgRoom.findById(req.params.id);
+
+    if (!room) return res.status(404).json({ error: "Room not found" });
+
+    let updatedImages = room.images;
+    const { imagesToDelete } = req.body;
+    const newImages = req.files ? req.files.map((file) => file.path) : [];
+
+    if (imagesToDelete && imagesToDelete.length > 0) {
+      await Promise.all(
+        imagesToDelete.map(async (imageUrl) => {
+          const publicId = imageUrl.split("/").pop().split(".")[0];
+          await cloudinary.uploader.destroy(`pg_images/${publicId}`);
+          updatedImages = updatedImages.filter((img) => img !== imageUrl);
+        })
+      );
+    }
+
+    updatedImages = [...updatedImages, ...newImages];
+
+    if (updatedImages.length > 5) {
+      updatedImages = updatedImages.slice(updatedImages.length - 5);
+    }
 
     const updatedRoom = await PgRoom.findByIdAndUpdate(
       req.params.id,
-      { ...req.body, ...(imageUrls && { images: imageUrls }) },
+      { ...req.body, images: updatedImages },
       { new: true }
     );
     res.json(updatedRoom);
@@ -59,7 +81,7 @@ const deletePg = async (req, res) => {
       return res.status(404).json({ error: "Room not found" });
     }
 
-    for(const imageUrl of room.images) {
+    for (const imageUrl of room.images) {
       const publicId = imageUrl.split("/").pop().split(".")[0];
       await cloudinary.uploader.destroy(publicId);
     }
@@ -73,7 +95,7 @@ const deletePg = async (req, res) => {
 
 const searchPgs = async (req, res) => {
   try {
-    const { query } = req.query; 
+    const { query } = req.query;
 
     if (!query) {
       return res.status(400).json({ message: "Search query is required" });
