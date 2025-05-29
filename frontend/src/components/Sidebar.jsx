d928e5d3f3304d04188a1ce5fff99683c5b51164
Glen Jayson Dmello <glendmello04@gmail.com>
2025-05-29 19:47:31 +0530

    implemented visibility function for admin button of sidebar for role based

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoSettingsSharp } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import { SiChatbot } from "react-icons/si";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
 import { FaHome } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const { role } = useSelector((store) => store.auth);
+  const [visibleItems, setVisibleItems] = useState([]);
 
   const items = [
     {
@@ -37,9 +40,21 @@ const Sidebar = () => {
     },
   ];
 
+  useEffect(() => {
+    if (role != "admin") {
+      setVisibleItems(filterOutAdmin());
+    } else {
+      setVisibleItems(items);
+    }
+  }, [role]);
+
+  const filterOutAdmin = () => {
+    return items.filter((item) => item.text != "Admin");
+  };
+
   return (
     <div className="fixed top-0 left-0 w-20 bg-black/30 backdrop-blur-lg flex flex-col items-center px-14 py-6 space-y-6 shadow-[0_8px_32px_rgb(0_0_0/0.5)] z-40 animate-fadeIn mt-32 rounded-2xl ml-5">
-      {items.map((item, index) => (
+      {visibleItems.map((item, index) => (
         <div
           key={index}
           className="flex flex-row items-center text-gray-300 hover:text-white transition-colors duration-300 cursor-pointer animate-slideFromLeft p-5 space-x-4 space-y-2 mr-4 ml-4"
