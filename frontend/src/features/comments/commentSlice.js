a5a376f932018fbab3e455c317b7855088fb521d
Glen <glendmello04@gmail.com>
2025-07-26 23:11:46 +0530

    commit

--- frontend/src/features/comments/commentSlice.js
+++ frontend/src/features/comments/commentSlice.js
@@ -1,8 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
 // const API_URL = "http://localhost:5000/api/comments";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 const API_URL = `${backendUrl}/api/comments`;
 
 const getAuthHeaders = () => ({
