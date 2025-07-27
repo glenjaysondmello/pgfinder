16f92dd601bda0a3b78b10cf0ccfc3bbdd988b95
Glen <glendmello04@gmail.com>
2025-07-27 15:01:41 +0530

    role correction

--- backend/controllers/userRoleController.js
+++ backend/controllers/userRoleController.js
@@ -4,23 +4,24 @@ const db = admin.firestore();
 
 const getUserRole = async (req, res) => {
   try {
-    
     const { uid } = req.params;
 
     if (!uid) {
-      return res.status(401).json({ error: "Unauthorized: No user ID provided" });
+      return res
+        .status(401)
+        .json({ error: "Unauthorized: No user ID provided" });
     }
 
     const userDoc = await db.collection("users").doc(uid).get();
 
     if (!userDoc.exists) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(200).json({ role: "user" });
     }
 
     const userData = userDoc.data();
-    const role = userData?.role || "user"; 
+    const role = userData?.role || "user";
 
-    return res.json({ role });
+    return res.status(200).json({ role });
   } catch (error) {
     console.error("Error Fetching the user role:", error);
     return res.status(500).json({ error: "Internal server error" });
