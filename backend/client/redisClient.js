a2c77dcfb2bfef61c44bfa30692d2d617fe339ff
Glen Jayson Dmello <glendmello04@gmail.com>
2025-08-19 18:55:19 +0530

    dockerization success for pg backend

--- backend/client/redisClient.js
+++ backend/client/redisClient.js
@@ -1,8 +1,16 @@
 const { createClient } = require("redis");
 
+
+const REDIS_URL =
+  `redis://redis:6379`;
+
 const redisClient = createClient({
-  url: "redis://127.0.0.1:6379",
+  url: REDIS_URL,
 });
+
+// const redisClient = createClient({
+//   url: "redis://127.0.0.1:6379",
+// });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
